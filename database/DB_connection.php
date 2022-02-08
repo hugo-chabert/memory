87bf8765ac1bde09a0f1d02ccc74cbd080f9beba
francois-niang1 <francois.niang@laplateforme.io>
2022-02-08 15:41:09 +0100

    done

--- database/DB_connection.php
+++ database/DB_connection.php
@@ -5,7 +5,7 @@ class Database
     public static function connect_db(): PDO
     {
         try {
-            $bdd = new PDO("mysql:host=localhost;dbname=memory;charset=utf8", "root", "root");
+            $bdd = new PDO("mysql:host=localhost;dbname=francois-niang_memory;charset=utf8", "francois-niangme", "memorymdp");
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if (!$bdd) {
                 die("Connexion a la bdd impossible");
