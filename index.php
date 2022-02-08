a72e3995e45cd6fe4c118d0c8ce606988650e428
francois-niang1 <francois.niang@laplateforme.io>
2022-02-08 13:00:49 +0100

    index terminer

--- index.php
+++ index.php
@@ -13,12 +13,20 @@ if (isset($_SESSION['user'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+        <link rel="stylesheet" href="public/css/index.css">
+    <link rel="stylesheet" href="public/css/root&font.css">
     <title>Memory</title>
 </head>
 <body>
     <?php require 'view/header-index.php'?>
     <main>
-        <?php var_dump($_SESSION)?>
+        <h1 class="block-effect" style="--td: 1.2s">
+            <div class="block-reveal" style="--bc: var(--HoverVar-); --d: .1s">Welcome</div>
+            <div class="block-reveal" style="--bc: var(--BaseVar-); --d: .5s">To <a href="view/jeu.php">Memory</a> </div>
+            <?php if (isset($_SESSION['user'])){ ?>
+                <div class="block-reveal" style="--bc: var(--HoverVar-); --d: .9s">
+                <?php echo $_SESSION['user']['login'];}?> </div>
+        </h1>
     </main>
     <?php require 'view/footer-index.php'?>
 </body>
